3d4986d53649aeb7737894240d8fef9742bfa5eb
Fannar Haraldsson <fannarhrafn@gmail.com>
2019-09-05 16:09:11 +0000

    Finished algorithm

--- sequence.py
+++ sequence.py
@@ -1,14 +1,24 @@
 #sequence.py
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
-count_number = 1
+count_number = 0
 num1 = 1
 num2 = 2
 num3 = 3
 while  count_number <= n:
-    summa = num1+num2+num3
-    print(summa)
-    count_number += 1
 
-    num1=num2
-    num2=num3
-    num3=summa
+    if count_number == 0:
+        summa = num1
+        print(summa)
+    elif count_number == 1:
+        summa = num1+1
+        print(summa)
+    elif count_number == 2:
+        summa = num1+num2
+        print(summa)
+    else:
+        summa = num1+num2+num3
+        num1=num2
+        num2=num3
+        num3=summa
+        print(summa)
+    count_number += 1
