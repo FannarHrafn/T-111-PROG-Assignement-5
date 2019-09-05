6891d5ce5b61577b8916fa2bc51acd64e11a6d00
Fannar Haraldsson <fannarhrafn@gmail.com>
2019-09-05 15:02:09 +0000

    added teacher given code

--- sequence.py
+++ sequence.py
@@ -1,1 +1,14 @@
 #sequence.py
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+count_number = 1
+num1 = 1
+num2 = 2
+num3 = 3
+while  count_number <= n:
+    summa = num1+num2+num3
+    print(summa)
+    count_number += 1
+
+    num1=num2
+    num2=num3
+    num3=summa
